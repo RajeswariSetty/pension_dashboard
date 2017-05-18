4ce66b3084e4d8f0f3a0d6e14320aa19174a907e
techaem dev <techaem.dev@gmail.com>
2017-05-18 17:44:08 +0530

    Added apps and client liraries for login and dashboard

--- ui.apps/src/main/content/jcr_root/apps/pensiondashboard/components/structure/employeedashboard/content.jsp
+++ ui.apps/src/main/content/jcr_root/apps/pensiondashboard/components/structure/employeedashboard/content.jsp
@@ -4,9 +4,20 @@ Employee Dashboard
 <%@include file="/libs/foundation/global.jsp"%>
 
 <cq:include path="par" resourceType="foundation/components/parsys"/>
-Name:<%= resourceResolver.adaptTo(Session.class).getUserID() %>
-<form id="loginModalForm1">
-<input type="text" name="amount"/>
-</form>
+Name:<%= resourceResolver.adaptTo(Session.class).getUserID() %><br/>
+
+Pensions found <label id="potCount"></label> <br/>
+Pensions income <label id="pensionIncomeMonthly"></label> monthly at age <label id="currentAge"></label><br/>
+Pensions income <label id="pensionIncomeAnnual"></label> monthly at age <label id="retirementAge"></label><br/>
+
+My retirement plan<br/>
+current plan<br/>
+<br/>
+Age  <label id="currentAge2"></label> Monthly salary  <label id="monthlySalCurrentAge"></label><br/>
+Pension Age  <label id="retirementAge2"></label> Target Pension Pot  <label id="targetPensionPot"></label><br/>
+Estimated monthly income including State Pension <label id="est_mnthly_income"></label> monthly<br/>
+
+My Actions <br/>
+<label id="actions"></label>
 
 <cq:includeClientLib js="employeedashboard.main"/>
--- ui.apps/src/main/content/jcr_root/apps/pensiondashboard/components/structure/employeedashboard/employeedashboard/js/employee-dashboard.js
+++ ui.apps/src/main/content/jcr_root/apps/pensiondashboard/components/structure/employeedashboard/employeedashboard/js/employee-dashboard.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
-//alert('Employee Dashboard');
-     $("#loginModalForm1  [name='amount']").val(1212);
-
-
-    $.ajax({
+   $.ajax({
          type: 'POST',    
          url:'/bin/employeedashboardServlet',
          success: function(msg){
-           alert(msg); //display the data returned by the servlet
+           //alert(msg); //display the data returned by the servlet
+              var json = jQuery.parseJSON(msg);
+              $('#potCount').val(json.potCount); 
+              $('#pensionIncomeMonthly').text(json.pensionIncomeMonthly);
+              $('#currentAge').text(json.currentAge);
+              $('#currentAge2').text(json.currentAge);
+              $('#retirementAge').text(json.retirementAge);
+              $('#retirementAge2').text(json.retirementAge);
+              $('#pensionIncomeAnnual').text(json.pensionIncomeAnnual);
+              $('#monthlySalCurrentAge').text(json.monthlySalCurrentAge);
+              $('#targetPensionPot').text(json.targetPensionPot);
+              $('#est_mnthly_income').text(json.est_mnthly_income);
+
+             var actions = "";
+
+             for(var i=0;i<json.actions.length;i++){
+				actions = actions+","+JSON.stringify(json.actions);
+             }
+             $('#actions').text(actions);
          }
      });
 
